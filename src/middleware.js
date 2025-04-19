c9ce36d31ef9a6afb07b86bc8287c531cbf99eab
RudraMaywad13 <108661516+RudraMaywad13@users.noreply.github.com>
2025-04-19 16:22:24 +0530

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -5,9 +5,6 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Protect all routes except static files and Next.js internals
-    "/((?!_next|.*\\.(?:png|jpg|jpeg|svg|css|js|json|ico|woff2?|ttf|map)).*)",
-    // Always protect API routes
-    "/(api|trpc)(.*)",
+   
   ],
 };
