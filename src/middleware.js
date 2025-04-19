d08448cbb9727d4dccddbd492bb4b5b718d6247c
RudraMaywad13 <108661516+RudraMaywad13@users.noreply.github.com>
2025-04-19 16:36:00 +0530

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,13 @@
 // middleware.js
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware();
 
 export const config = {
   matcher: [
-   
+    '/',
+    '/solver',
+    '/rules',
+    '/api/:path*',
   ],
 };
